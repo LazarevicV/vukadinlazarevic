0b4dad3f385267f95dd77b2d76889469d1a2118e
LazarevicV <vukadinlazarevic@gmail.com>
2023-10-16 03:39:35 +0200

    homepage cleaned, now uses components

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,12 @@
-import { Main as NextMain } from "next/document";
 import MainPageBorder from "./_components/MainPageBorder";
+import NavigationMenu from "./_components/NavigationMenu";
+import ContentSection from "./_components/ContentSection";
 
 export default function Home() {
   return (
     <MainPageBorder>
-      <header className="bg-gray-800 py-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-white">My Portfolio</h1>
-          <nav className="mt-4 flex justify-between">
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Home
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Projects
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            >
-              About
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Contact
-            </a>
-          </nav>
-        </div>
-      </header>
+      <NavigationMenu />
+      <ContentSection />
     </MainPageBorder>
   );
 }
